ae54f4a725e81b58e0042853c7f3a1b3ed4bc1db
oyramn <39172186+oyramn@users.noreply.github.com>
2018-08-14 18:41:22 -0400

    Update install.R

--- install.R
+++ install.R
@@ -3,5 +3,4 @@ install.packages("prettyR")
 install.packages("knitr")
 install.packages("devtools")
 install.packages("BiocInstaller", repos = "http://www.bioconductor.org/packages/3.5/bioc")
-install_github("hms-dbmi/Rcupcake", force = TRUE)
 
