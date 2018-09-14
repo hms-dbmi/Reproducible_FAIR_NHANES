77c278dda6dac0fffd9b54d2cca157332c28cdff
oyramn <39172186+oyramn@users.noreply.github.com>
2018-09-14 17:04:31 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,6 +1,2 @@
 install.packages("dplyr")
 install.packages("prettyR")
-install.packages("knitr")
-install.packages("devtools")
-install.packages("BiocInstaller", repos = "http://www.bioconductor.org/packages/3.5/bioc")
-
